1fc8a307ed30131008665aca75798820f00ea1c4
Parham Alvani <parham.alvani@gmail.com>
2015-07-02 08:44:27 +0430

    Project structure is added.

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : main
+ *
+ * [] Creation Date : 02-07-2015
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2015 Parham Alvani.
+*/
+
+int main(int argc, char *argv[])
+{
+
+}
