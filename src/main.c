a5abf36c7450df76ca35c34f8e2e805d16675547
Parham Alvani <parham.alvani@gmail.com>
2015-07-02 09:08:06 +0430

    Coordinator handling is done.

--- src/main.c
+++ src/main.c
@@ -12,7 +12,30 @@
  * Copyright (c) 2015 Parham Alvani.
 */
 
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "common.h"
+
+int is_coordinator = 0;
+
 int main(int argc, char *argv[])
 {
+	int coordinator_fd;
+	FILE *coordinator_file;
 
+	coordinator_fd = open("/tmp/coordinator", O_EXCL | O_CREAT | O_WRONLY,
+		0664);
+	if (coordinator_fd > 0) {
+		printf("New coordinator is selected\n");
+		is_coordinator = 1;
+		coordinator_file = fdopen(coordinator_fd, "w");
+	}
+
+	if (is_coordinator) {
+		if (unlink("/tmp/coordinator") < 0)
+			sdie("unlink()");
+		printf("Coordinator died");
+	}
 }
