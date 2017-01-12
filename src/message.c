e453a9c513ccfa28e9c984e68b84e2b94efc7cba
Parham Alvani <parham.alvani@gmail.com>
2017-01-12 17:51:47 +0330

    Implements message serialization and de-serialization.

---
+++ src/message.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "common.h"
+#include "message.h"
+
+int message_serialize(FILE *dest, const struct message *message)
+{
+	int return_value = fprintf(dest, "%s %s %zu\n%s",
+		message->src_name, message->dst_name,
+		message->m_size, message->body);
+	
+	if (return_value < 0)
+		sdie("message_serialize printf()");
+
+	fflush(dest);
+	return return_value;
+}
+
+int message_deserialize(FILE *src, struct message *message)
+{
+	int return_value = 0;
+
+	if (fscanf(src, "%s", message->src_name) != 1)
+		return -1;
+	return_value += strlen(message->src_name);
+
+	if (fscanf(src, "%s", message->dst_name) != 1)
+		return -1;
+	return_value += strlen(message->dst_name);
+
+	if (fscanf(src, "%zu", &message->m_size) != 1)
+		return -1;
+
+	message->body = calloc(message->m_size + 1, sizeof(char));
+
+	/* Read message body start indicator ('\n') */
+	fgetc(src);
+	return_value += fread(message->body, message->m_size,
+		sizeof(char), src);
+
+	return return_value;
+}
